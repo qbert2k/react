402d52b44da58d3b6a45c7b7361f9bbdae0bbe13
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-01-13 00:11:27 -0400

    CounterApp: useState Hook

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,17 +1,21 @@
+import React, {useState} from "react";
 import PropTypes from 'prop-types'
 import {Fragment} from "react";
 
 const CounterApp = ({value}) => {
 
+    const [counter, setCounter] = useState(0);
+
     // handleAdd
-    const handleAdd = (e) => {
-        console.log(e);
+    const handleAdd = () => {
+        setCounter(counter + 1);
+        // setCounter((c) => c + 1);
     };
 
     return (
         <Fragment>
             <h1>CounterApp</h1>
-            <h2> {value} </h2>
+            <h2> {counter} </h2>
 
             <button onClick={handleAdd}>+1</button>
         </Fragment>
