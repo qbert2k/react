fba19812b1a1aefc3cee83115963ab217417e23e
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-01-12 23:50:37 -0400

    CounterApp: Evento CLick (Eventos en general)

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -2,10 +2,18 @@ import PropTypes from 'prop-types'
 import {Fragment} from "react";
 
 const CounterApp = ({value}) => {
+
+    // handleAdd
+    const handleAdd = (e) => {
+        console.log(e);
+    };
+
     return (
         <Fragment>
             <h1>CounterApp</h1>
             <h2> {value} </h2>
+
+            <button onClick={handleAdd}>+1</button>
         </Fragment>
     );
 };
@@ -15,3 +23,5 @@ CounterApp.prototypes = {
 }
 
 export default CounterApp;
+
+// https://es.reactjs.org/docs/events.html
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -5,4 +5,4 @@ import './index.css';
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render(<CounterApp value={123456789}/>, divRoot);
+ReactDOM.render(<CounterApp value={10}/>, divRoot);
