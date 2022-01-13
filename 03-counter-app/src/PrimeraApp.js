013cc2b0e5b1f78be82e8c46a2facd8b45839cba
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-01-12 23:10:13 -0400

    CounterApp: Impresión de variables en el HTML

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -2,9 +2,13 @@ import React from "react";
 // import React, {Fragment} from "react";
 
 const PrimeraApp = () => {
+
+    const saludo = 'Hola Mundo';
+
     return (
         <>
-            <h1>Hola Mundo</h1>
+            <h1>{saludo}</h1>
+            {/*<pre> {JSON.stringify(saludo, null, 3)} </pre>*/}
             <p>Mi primera aplicacion</p>
         </>
     );
