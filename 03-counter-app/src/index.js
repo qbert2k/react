5fd806d4b11dd3fa254c5fdca6eb434ebbeeb67c
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-01-13 22:51:24 -0400

    Pruebas unitarias y de integración - Probando las secciones anteriores
    Pruebas sobre componentes de React

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import CounterApp from "./CounterApp";
 import './index.css';
+import PrimeraApp from "./PrimeraApp";
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render(<CounterApp value={10}/>, divRoot);
+ ReactDOM.render(<PrimeraApp saludo="Hola soy Goku"/>, divRoot);
+// ReactDOM.render(<CounterApp value={10}/>, divRoot);
---
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {render} from "@testing-library/react";
+import PrimeraApp from "../PrimeraApp";
+
+describe('Pruebas en <PrimeraApp/>', () => {
+    test('Debe mostrar el mensaje Hola, soy Goku', () => {
+        const saludo = 'Hola, soy Goku';
+
+        const {getByText} = render(<PrimeraApp saludo={saludo}/>);
+
+        expect(getByText(saludo)).toBeInTheDocument();
+    })
+})
