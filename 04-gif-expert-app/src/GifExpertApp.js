73eb1319a4602e0e70b6dd093330df6946f7da1a
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-01-14 23:06:26 -0400

    GifExpertApp: Component

---
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const GifExpertApp = () => {
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr/>
+        </>
+    );
+};
+
+// https://es.reactjs.org/docs/faq-structure.html
+// https://hackernoon.com/structuring-projects-and-naming-components-in-react-1261b6e18d76
