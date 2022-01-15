3951c0317dc5aaf886da0f10ec43e55947002788
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-01-15 00:14:46 -0400

    GifExpertApp: useEffect

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 export const GifGrid = ({category}) => {
 
-    const getGif = async () => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        getGifs();
+    }, []);
+
+    const getGifs = async () => {
         const url = `https://api.giphy.com/v1/gifs/search?q=Rick and Morty&limit=10&api_key=DXGF5aEdYUiPebwCAF7ILESVrmeXHI0M`;
         const respuesta = await fetch(url);
         const {data} = await respuesta.json();
@@ -18,11 +24,11 @@ export const GifGrid = ({category}) => {
         console.log(gifs);
     }
 
-    getGif();
-
     return (
         <div>
             <h3>{category}</h3>
+            <h3>{count}</h3>
+            <button onClick={() => setCount(count + 1)}>a</button>
         </div>
     );
 }
