92b4c5a2eed03f630eb7d698d852169b6cc0ea75
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-01-15 22:42:50 -0400

    Generando el build de producción y despliegues en Github Pages

---
+++ 04-gif-expert-app/docs/precache-manifest.01da54199d73ad715b8d7963b9a1ca09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774cb7164e3e83ca0b650ba68a08658e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4c2143babf76a168d71",
+    "url": "/static/css/main.7200259e.chunk.css"
+  },
+  {
+    "revision": "e107496e2e01402b74e7",
+    "url": "/static/js/2.b11609f6.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.b11609f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c2143babf76a168d71",
+    "url": "/static/js/main.58f366ab.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
