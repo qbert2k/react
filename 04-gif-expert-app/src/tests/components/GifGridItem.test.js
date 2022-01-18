f60be728ae6f07f273e91a229b0d50a8dbcfd761
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-01-17 21:46:20 -0400

    Testing - Probando la aplicación de GifExpert
    Pruebas del componente - GifGridItem

--- 04-gif-expert-app/src/tests/components/GifGridItem.test.js
+++ 04-gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -6,9 +6,29 @@ describe('Pruebas en <GifGridItem/>', () => {
 
     const title = 'Un título';
     const url = 'https://localhost/algo.jpg';
+    const wrapper = shallow(<GifGridItem title={title} url={url}/>);
 
     test('Debe mostrar el componente correctamente', () => {
-        const wrapper = shallow(<GifGridItem title={title} url={url}/>);
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test('debe de tener un párrafo con el título', () => {
+        const p = wrapper.find('p');
+
+        expect(p.text().trim()).toBe(title);
+    });
+
+    test('debe de tener la imagen igual al url y alt de los props', () => {
+        const img = wrapper.find('img');
+
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    });
+
+    test('debe tener animate__fadeIn', () => {
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+
+        expect(className.includes('animate__fadeIn')).toBe(true);
     });
 });
