cb0fe1a0cc977fd1b894ad2122549c43d6a695e4
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-01-17 22:32:12 -0400

    Testing - Probando la aplicación de GifExpert
    Simular un submit del formulario
    Simular submit llamando la función y limpiando el inputValue

--- 04-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -4,10 +4,13 @@ import {AddCategory} from "../../components/AddCategory";
 
 describe('Pruebas en <AddCategory/>', () => {
 
-    const setCategories = () => {
-    };
+    const setCategories = jest.fn();
+    let wrapper;
 
-    const wrapper = shallow(<AddCategory setCategories={setCategories}/>);
+    beforeEach(() => {
+        jest.clearAllMocks();
+        wrapper = shallow(<AddCategory setCategories={setCategories}/>);
+    });
 
     test('debe de mostrarse correctamente', () => {
         expect(wrapper).toMatchSnapshot();
@@ -22,5 +25,39 @@ describe('Pruebas en <AddCategory/>', () => {
         });
 
         expect(wrapper.find('p').text().trim()).toBe(value);
+    });
+
+    test('No debe de postear la información con submit', () => {
+        wrapper.find('form').simulate('submit', {
+            preventDefault() {
+            }
+        });
+
+        expect(setCategories).not.toHaveBeenCalled();
+    });
+
+    test('debe de llamar el setCategories y limpiar la caja de texto', () => {
+        const value = 'Hola que hacen?';
+        const input = wrapper.find('input');
+
+        // 1. Simular el inputChange
+        input.simulate('change', {
+            target: {value}
+        });
+
+        // 2. Simular el submit
+        wrapper.find('form').simulate('submit', {
+            preventDefault() {
+            }
+        });
+
+        // 3. setCategories se debe de haver llamado
+        expect(setCategories).toHaveBeenCalled();
+        expect(setCategories).toHaveBeenCalledTimes(1);
+        expect(setCategories).toHaveBeenCalledWith(expect.any(Function));
+
+        // 4. el valor del input debe de estar ''
+        expect(input.text()).toBe('');
+        expect(input.prop('value')).toBe('');
     });
 });
