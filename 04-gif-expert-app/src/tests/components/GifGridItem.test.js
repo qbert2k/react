8bbbedba3a65d3393ee774f66a9e33775ff1899d
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-01-17 21:33:30 -0400

    Testing - Probando la aplicación de GifExpert
    Configurar el ambiente de pruebas
    Implementando PropTypes y Actualizar Snapshots

---
+++ 04-gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {shallow} from 'enzyme';
+import {GifGridItem} from "../../components/GifGridItem";
+
+describe('Pruebas en <GifGridItem/>', () => {
+
+    const title = 'Un título';
+    const url = 'https://localhost/algo.jpg';
+
+    test('Debe mostrar el componente correctamente', () => {
+        const wrapper = shallow(<GifGridItem title={title} url={url}/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
