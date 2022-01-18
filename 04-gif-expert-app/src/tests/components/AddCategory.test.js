630c23b3810ad9278932ea6cb4269db072e4d840
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-01-17 22:06:49 -0400

    Testing - Probando la aplicación de GifExpert
    Simular cambios en un input

--- 04-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -7,9 +7,20 @@ describe('Pruebas en <AddCategory/>', () => {
     const setCategories = () => {
     };
 
-    test('debe de mostrarse correctamente', () => {
-        const wrapper = shallow(<AddCategory setCategories={setCategories}/>);
+    const wrapper = shallow(<AddCategory setCategories={setCategories}/>);
 
+    test('debe de mostrarse correctamente', () => {
         expect(wrapper).toMatchSnapshot();
-    })
+    });
+
+    test('debe de cambiar la caja de texto', () => {
+        const input = wrapper.find('input');
+        const value = 'Hola Mundo';
+
+        input.simulate('change', {
+            target: {value}
+        });
+
+        expect(wrapper.find('p').text().trim()).toBe(value);
+    });
 });
