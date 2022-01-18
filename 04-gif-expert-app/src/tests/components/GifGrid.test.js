150a64d1a56c5d110c5367fd01cd70b0293bd06e
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-01-17 22:59:56 -0400

    Testing - Probando la aplicación de GifExpert
    Pruebas del componente GifGrid - Mock customHook

---
+++ 04-gif-expert-app/src/tests/components/GifGrid.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {shallow} from 'enzyme'
+import {GifGrid} from '../../components/GifGrid';
+import {useFetchGifs} from '../../hooks/useFetchGifs';
+
+jest.mock('../../hooks/useFetchGifs');
+
+describe('Prueba en el <GifGrid/>', () => {
+
+    const category = 'Nirvana';
+
+    test('debe mostrarse correctamente', () => {
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true
+        });
+
+        const wrapper = shallow(<GifGrid category={category}/>);
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('debe de mostrar items cuando se cargan imágenes useFetchGifs', () => {
+        const gifs = [{
+            id: 'ABC',
+            url: 'https://localhost/cualquier/cosa.jpg',
+            title: 'Cualquier cosa'
+        }];
+        useFetchGifs.mockReturnValue({
+            data: gifs,
+            loading: false
+        });
+
+        const wrapper = shallow(<GifGrid category={category}/>);
+
+        expect(wrapper).toMatchSnapshot();
+    });
+})
