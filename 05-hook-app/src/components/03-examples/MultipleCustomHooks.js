db03e3c22c4e77bf7b8b44e79a9c5f77271f8562
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-01-31 00:16:08 -0400

    HookApp: useFetch + useCounter

--- 05-hook-app/src/components/03-examples/MultipleCustomHooks.js
+++ 05-hook-app/src/components/03-examples/MultipleCustomHooks.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import '../02-useEffect/effects.css';
-import {useFetch} from "../../hooks/useFetch";
+import {useFetch} from '../../hooks/useFetch';
+import {useCounter} from '../../hooks/useCounter';
 
 export const MultipleCustomHooks = () => {
 
-    const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-    console.log(state);
+    const {counter, increment} = useCounter(1);
+    const {loading, data} = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+    const {author, quote} = !!data && data[0];
 
     return (
         <div>
-            <h1>Multiple custom Hooks!!!</h1>
+            <h1>BreakingBad Quotes</h1>
+            <hr/>
+
+            {
+                loading ? (
+                    <div className="alert alert-info text-center">
+                        Loading...
+                    </div>
+                ) : (
+                    <blockquote className="blockquote text-right">
+                        <p className="mb-0">{quote}</p>
+                        <br/>
+                        <footer className="blockquote-footer">
+                            {author} in
+                            <cite title="BreakingBad">BreakingBad</cite>
+                        </footer>
+                    </blockquote>
+                )
+            }
+
+            <button
+                className="btn btn-primary"
+                onClick={increment}>
+                Next quote
+            </button>
         </div>
     );
 }
