e83739bf32963478b50561523a5bcff9fd1f52b3
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-01-30 22:39:50 -0400

    HookApp: useCounter

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {CounterApp} from "./components/01-useState/CounterApp";
+import {CounterWithCustomHook} from "./components/01-useState/CounterWithCustomHook";
 
 ReactDOM.render(
-    <CounterApp/>,
+    <CounterWithCustomHook/>,
     document.getElementById('root')
 );
