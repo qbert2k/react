12203beb13cfe1aa755ec3b8e0e05c1e0256944f
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-02-12 23:48:05 -0400

    HookApp: useMemo

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Memorize} from './components/06-memos/Memorize';
+import {MemoHook} from './components/06-memos/MemoHook';
 
 ReactDOM.render(
-    <Memorize/>,
+    <MemoHook/>,
     document.getElementById('root')
 );
