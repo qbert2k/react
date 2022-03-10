8c17effe6afa2939a9881935534c3ec6ad82d97c
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-03-09 23:00:58 -0400

    useReducer: Idea general de un reducer

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Padre} from './components/07-tarea-memo/Padre';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import {Padre} from './components/07-tarea-memo/Padre';
+//
+// ReactDOM.render(
+//     <Padre/>,
+//     document.getElementById('root')
+// );
 
-ReactDOM.render(
-    <Padre/>,
-    document.getElementById('root')
-);
+import './components/08-useReducer/intro-reducer';
