3f8fcbe7fe8c36cc1114d12999e3661a08bf9c69
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-04-05 23:13:21 -0400

    useContext: CreateContext y useContext

--- 05-hook-app/src/components/09-useContext/MainApp.js
+++ 05-hook-app/src/components/09-useContext/MainApp.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import AppRouter from './AppRouter';
+import {UserContext} from './UserContext';
 
 const MainApp = () => {
+    const user = {
+        id: 1234,
+        name: 'javier',
+        email: 'javier.rojas.blum@gmail.com'
+    };
+
     return (
-        <AppRouter/>
+        <UserContext.Provider value={user}>
+            <AppRouter/>
+        </UserContext.Provider>
     );
 };
 
