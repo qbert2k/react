61b822277d9e9790bdafa2c417f22bed39ca3669
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2022-04-06 23:54:34 -0400

    Pruebas unitarias y de integracion - Hooks
    Pruebas sobre useFetch CustomHook

--- 05-hook-app/src/hooks/useFetch.js
+++ 05-hook-app/src/hooks/useFetch.js
@@ -24,10 +24,15 @@ export const useFetch = (url) => {
                         error: null,
                         data: data
                     });
-                } else {
-                    console.log('setState no se llamo');
                 }
             })
+            .catch(() => {
+                setState({
+                    data: null,
+                    loading: false,
+                    error: 'No se pudo cargar la info'
+                })
+            });
     }, [url])
 
     return state;
