35f06de7bcdd13b689affb5ff9e371efc8201dad
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-03-09 22:15:21 +0000

    Impresion de variables en el HTML

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,8 +1,16 @@
+const newMessage = {
+    message: 'Hola Mundo',
+    title: 'title'
+};
+
+const getResult = (a, b) => a + b;
+
 export const FirstApp = () => {
-    return (
-        <>
-            <h1>Javier</h1>
-            <p>Soy un subtitulo</p>
-        </>
-    );
+    return (<>
+        <h1>{newMessage.title}</h1>
+        <code>{JSON.stringify(newMessage)}</code>
+        <p>Soy un texto</p>
+
+        <p>{getResult(5, 6)}</p>
+    </>);
 }
