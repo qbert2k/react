2d333a57e4703eaeae936370044813c120b87a52
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-03-11 23:14:06 +0000

    Pruebas unitarias y de integracion

--- 03-counter-app-vite/README.md
+++ 03-counter-app-vite/README.md
@@ -12,6 +12,16 @@ Run in powershell as administrator
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 ```
 
+# Setup Linux
+
+Disabling IPv6
+
+```shell
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+```
+
 Download dependencies
 
 ```shell
@@ -42,10 +52,6 @@ yarn add --dev babel-jest @babel/core @babel/preset-env
 yarn add -D whatwg-fetch 
 yarn add --dev @testing-library/react
 yarn add -D jest-environment-jsdom
-
-
-
-
 
 yarn add -D @babel/preset-react
 ```
--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,27 +1,27 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import PropTypes from 'prop-types';
 
-export const CounterApp = ({ value }) => {
+export const CounterApp = ({value}) => {
 
-    const [ counter, setCounter ] = useState( value );
+    const [counter, setCounter] = useState(value);
 
     const handleAdd = () => {
         // console.log(event)
-        setCounter( counter + 1 );
+        setCounter(counter + 1);
         // setCounter( (c) => c + 1 )
     }
 
-    const handleSubstract = () => setCounter( counter - 1 );
-    const handleReset = () => setCounter( value );
+    const handleSubstract = () => setCounter(counter - 1);
+    const handleReset = () => setCounter(value);
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { counter } </h2>
+            <h2>{counter}</h2>
 
-            <button onClick={ handleAdd }> +1 </button>
-            <button onClick={ handleSubstract }> -1 </button>
-            <button aria-label="btn-reset" onClick={ handleReset }> Reset </button>
+            <button onClick={handleAdd}>+1</button>
+            <button onClick={handleSubstract}>-1</button>
+            <button aria-label="btn-reset" onClick={handleReset}>Reset</button>
         </>
     );
 }
