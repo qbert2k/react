9232b69b52cb9af59e1d750e701649cb7ebc83cb
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-03-16 19:44:45 +0000

    Hook App - Project Setup

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import {HooksApp} from "./HooksApp.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <HooksApp/>
+    </React.StrictMode>,
 )
