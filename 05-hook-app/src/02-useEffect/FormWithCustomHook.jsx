be39359c885238f26a17f0f27422df674ce8824d
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-03-18 21:08:00 +0000

    Hook App

--- 05-hook-app/src/02-useEffect/FormWithCustomHook.jsx
+++ 05-hook-app/src/02-useEffect/FormWithCustomHook.jsx
@@ -1,53 +1,51 @@
-import { useEffect } from 'react';
-import { useForm } from '../hooks/useForm';
-
+import {useEffect} from 'react';
+import {useForm} from '../hooks/useForm';
 
 export const FormWithCustomHook = () => {
 
-    const { formState, onInputChange, onResetForm, username, email, password } = useForm({
+    const {formState, onInputChange, onResetForm, username, email, password} = useForm({
         username: '',
         email: '',
         password: ''
     });
 
     // const { username, email, password } = formState;
-  
 
     return (
         <>
-            <h1>Formulario con custom Hook</h1> 
-            <hr />
+            <h1>Form with custom Hook</h1>
+            <hr/>
 
-            <input 
-                type="text" 
+            <input
+                type="text"
                 className="form-control"
                 placeholder="Username"
                 name="username"
-                value={ username }
-                onChange={ onInputChange }
+                value={username}
+                onChange={onInputChange}
             />
 
-            <input 
-                type="email" 
+            <input
+                type="email"
                 className="form-control mt-2"
-                placeholder="fernando@google.com"
+                placeholder="javier.rojas.blum@google.com"
                 name="email"
-                value={ email }
-                onChange={ onInputChange }
+                value={email}
+                onChange={onInputChange}
             />
 
-            <input 
-                type="password" 
+            <input
+                type="password"
                 className="form-control mt-2"
                 placeholder="Contraseña"
                 name="password"
-                value={ password }
-                onChange={ onInputChange }
+                value={password}
+                onChange={onInputChange}
             />
 
-
-            <button onClick={ onResetForm } className="btn btn-primary mt-2">Borrar</button>
-
+            <button onClick={onResetForm} className="btn btn-primary mt-2">
+                Clear
+            </button>
         </>
     )
 }
