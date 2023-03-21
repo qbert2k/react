01677f45dfcab0fe8e06e2c153ec9a6b9505bd91
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-03-21 21:27:02 +0000

    React Router Contacts - Adding Search Spinner

--- 00-react-router-contacts/src/routes/root.jsx
+++ 00-react-router-contacts/src/routes/root.jsx
@@ -19,6 +19,10 @@ export default function Root() {
     const navigation = useNavigation();
     const submit = useSubmit();
 
+    const searching =
+        navigation.location &&
+        new URLSearchParams(navigation.location.search).has("q");
+
     useEffect(() => {
         document.getElementById("q").value = q;
     }, [q]);
@@ -31,6 +35,7 @@ export default function Root() {
                     <Form id="search-form" role="search">
                         <input
                             id="q"
+                            className={searching ? "loading" : ""}
                             aria-label="Search contacts"
                             placeholder="Search"
                             type="search"
@@ -43,7 +48,7 @@ export default function Root() {
                         <div
                             id="search-spinner"
                             aria-hidden
-                            hidden={true}
+                            hidden={!searching}
                         />
                         <div
                             className="sr-only"
