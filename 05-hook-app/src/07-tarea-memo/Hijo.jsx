482e957c9e57a14b54afea23ac26ab458a19a316
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-03-24 21:51:39 +0000

    General Hooks

--- 05-hook-app/src/07-tarea-memo/Hijo.jsx
+++ 05-hook-app/src/07-tarea-memo/Hijo.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
-export const Hijo = React.memo(({ numero, incrementar }) => {
+export const Hijo = React.memo(({numero, incrementar}) => {
 
     console.log('  Me volví a generar :(  ');
 
     return (
-        <button
-            className="btn btn-primary mr-3"
-            onClick={ () => incrementar( numero ) }
-        >
-            { numero }
+        <button className="btn btn-primary mr-3"
+                onClick={() => incrementar(numero)}>
+            {numero}
         </button>
     )
 })
