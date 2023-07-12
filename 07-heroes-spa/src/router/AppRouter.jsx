a54079db04e8a771621b10bbccae09f58b6192b2
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-07-12 20:04:15 +0100

    Heroes SPA

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,6 +1,7 @@
 import {Navigate, Route, Routes} from 'react-router-dom';
 import {MarvelPage} from '../heroes/pages/MarvelPage.jsx';
 import {LoginPage} from '../auth/pages/LoginPage.jsx';
+import {DcPage} from '../heroes/pages/DcPage.jsx';
 
 export const AppRouter = () => {
     return (
