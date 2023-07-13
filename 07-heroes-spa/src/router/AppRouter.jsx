842112b13935a6ea38e6a850ca9eb1c4f4b82e68
Javier Rojas <javier.rojas@sita.aero>
2023-07-13 10:58:34 +0100

    Heroes SPA

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -2,10 +2,13 @@ import {Navigate, Route, Routes} from 'react-router-dom';
 import {MarvelPage} from '../heroes/pages/MarvelPage.jsx';
 import {LoginPage} from '../auth/pages/LoginPage.jsx';
 import {DcPage} from '../heroes/pages/DcPage.jsx';
+import {Navbar} from '../ui';
 
 export const AppRouter = () => {
     return (
         <>
+            <Navbar/>
+
             <Routes>
                 <Route path="marvel" element={<MarvelPage/>}/>
                 <Route path="dc" element={<DcPage/>}/>
