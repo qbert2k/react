60318ec8159e358f20547df23d85a4e40a50a71a
Javier Rojas <javier.rojas@sita.aero>
2023-07-13 16:18:03 +0100

    Heroes SPA

--- 07-heroes-spa/src/heroes/components/HeroCard.jsx
+++ 07-heroes-spa/src/heroes/components/HeroCard.jsx
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 export const HeroCard = ({
                              id,
                              superhero,
@@ -6,9 +8,43 @@ export const HeroCard = ({
                              first_appearance,
                              characters
                          }) => {
+
+    const heroImageUrl = `/assets/heroes/${id}.jpg`;
+
     return (
-        <div>
-Hero Card
+        <div className="col">
+            <div className="card">
+                <div className="row no-gutters">
+                    <div className="col-4">
+                        <img src={heroImageUrl} className="card-img" alt={superhero}/>
+                    </div>
+
+                    <div className="col-8">
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                {superhero}
+                            </h5>
+                            <p className="card-text">
+                                {alter_ego}
+                            </p>
+
+                            {
+                                (alter_ego !== characters) && (<p>{characters}</p>)
+                            }
+
+                            <p className="card-text">
+                                <small className="text-muted">
+                                    {first_appearance}
+                                </small>
+                            </p>
+
+                            <Link to={`/hero/${id}`}>
+                                Read More...
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
