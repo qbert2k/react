42864014759f2a7af5100f9a89b71bf6bd087913
Javier Rojas <javier.rojas@sita.aero>
2023-07-13 16:04:27 +0100

    Heroes SPA

---
+++ 07-heroes-spa/src/heroes/components/HeroCard.jsx
@@ -0,0 +1,14 @@
+export const HeroCard = ({
+                             id,
+                             superhero,
+                             publisher,
+                             alter_ego,
+                             first_appearance,
+                             characters
+                         }) => {
+    return (
+        <div>
+Hero Card
+        </div>
+    );
+}
