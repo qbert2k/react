c7b1dfa7b93e13ed123d94eb7c7a6f5f81025a07
Javier Rojas <javier.rojas@sita.aero>
2023-07-13 14:20:47 +0100

    Heroes SPA

--- 07-heroes-spa/src/ui/components/Navbar.jsx
+++ 07-heroes-spa/src/ui/components/Navbar.jsx
@@ -1,6 +1,15 @@
-import {Link, NavLink} from 'react-router-dom';
+import {Link, NavLink, useNavigate} from 'react-router-dom';
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/login', {
+            replace: true
+        });
+    };
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
 
@@ -34,7 +43,8 @@ export const Navbar = () => {
                         Javier
                     </span>
 
-                    <button className="nav-item nav-link btn">
+                    <button className="nav-item nav-link btn"
+                            onClick={onLogout}>
                         Logout
                     </button>
                 </ul>
