ee3466f1d9662408084270936d15053fb611f95d
Javier Rojas <javier.rojas@sita.aero>
2023-07-13 14:04:57 +0100

    Heroes SPA

---
+++ 07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -0,0 +1,5 @@
+export const HeroPage = () => {
+    return (
+        <h1>Hero Page</h1>
+    );
+}
--- 07-heroes-spa/src/ui/components/Navbar.jsx
+++ 07-heroes-spa/src/ui/components/Navbar.jsx
@@ -20,6 +20,11 @@ export const Navbar = () => {
                              to="/dc">
                         DC
                     </NavLink>
+
+                    <NavLink className={({isActive}) => `nav-item nav-link ${isActive ? 'active' : ''}`}
+                             to="/search">
+                        Search
+                    </NavLink>
                 </div>
             </div>
 
