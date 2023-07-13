820ec82a8167224a68c7ca6e39a91fe83d018124
Javier Rojas <javier.rojas@sita.aero>
2023-07-13 17:25:59 +0100

    Heroes SPA

--- 07-heroes-spa/src/heroes/pages/HeroPage.jsx
+++ 07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,13 @@
+import {useParams} from 'react-router-dom';
+import {getHeroById} from '../helpers';
+
 export const HeroPage = () => {
+
+    const {id} = useParams();
+    const hero = getHeroById(id);
+
+    console.log(hero);
+
     return (
         <h1>Hero Page</h1>
     );
