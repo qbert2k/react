f49990edd547bccf471a373bd1364926951ee603
Javier Rojas <javier.rojas@sita.aero>
2023-07-14 11:36:36 +0100

    Heroes SPA

--- 07-heroes-spa/src/heroes/components/HeroList.jsx
+++ 07-heroes-spa/src/heroes/components/HeroList.jsx
@@ -1,9 +1,13 @@
 import {getHeroesByPublisher} from '../helpers';
-import {HeroCard} from "./HeroCard.jsx";
+import {HeroCard} from './HeroCard.jsx';
+import {useMemo} from 'react';
 
 export const HeroList = ({publisher}) => {
 
-    const heroes = getHeroesByPublisher(publisher);
+    const heroes = useMemo(
+        () => getHeroesByPublisher(publisher),
+        [publisher]
+    );
 
     return (
         <div className="row row-cols-1 row-cols-md-3 g-3">
