3eed08b9085703d9d4026418fa6a01a2c6bc72af
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-07-15 14:16:39 +0100

    Heroes SPA

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -10,8 +10,10 @@ export const SearchPage = () => {
     const location = useLocation();
 
     const {q = ''} = queryString.parse(location.search);
-
     const heroes = getHeroesByName(q);
+
+    const showSearch = (q.length === 0);
+    const showError = (q.length > 0) && heroes.length === 0;
 
     const {searchText, onInputChange} = useForm({
         searchText: q
@@ -53,11 +55,19 @@ export const SearchPage = () => {
                     <h4>Results</h4>
                     <hr/>
 
-                    {
-                        (q === '')
-                            ? <div className="alert alert-primary">Search a hero</div>
-                            : (heroes.length === 0) && <div className="alert alert-danger">No hero with <b>{q}</b></div>
-                    }
+                    {/*{*/}
+                    {/*    (q === '')*/}
+                    {/*        ? <div className="alert alert-primary">Search a hero</div>*/}
+                    {/*        : (heroes.length === 0) && <div className="alert alert-danger">No hero with <b>{q}</b></div>*/}
+                    {/*}*/}
+
+                    <div className="alert alert-primary" style={{display: showSearch ? '' : 'none'}}>
+                        Search a hero
+                    </div>
+
+                    <div className="alert alert-danger" style={{display: showError ? '' : 'none'}}>
+                        No hero with <b>{q}</b>
+                    </div>
 
                     {
                         heroes.map(
