c66ea9591f98051b5ec91c97f4505ba433d6da9f
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-07-15 14:18:38 +0100

    Heroes SPA

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -55,17 +55,13 @@ export const SearchPage = () => {
                     <h4>Results</h4>
                     <hr/>
 
-                    {/*{*/}
-                    {/*    (q === '')*/}
-                    {/*        ? <div className="alert alert-primary">Search a hero</div>*/}
-                    {/*        : (heroes.length === 0) && <div className="alert alert-danger">No hero with <b>{q}</b></div>*/}
-                    {/*}*/}
-
-                    <div className="alert alert-primary" style={{display: showSearch ? '' : 'none'}}>
+                    <div className="alert alert-primary animate__animated animate__fadeIn"
+                         style={{display: showSearch ? '' : 'none'}}>
                         Search a hero
                     </div>
 
-                    <div className="alert alert-danger" style={{display: showError ? '' : 'none'}}>
+                    <div className="alert alert-danger animate__animated animate__fadeIn"
+                         style={{display: showError ? '' : 'none'}}>
                         No hero with <b>{q}</b>
                     </div>
 
