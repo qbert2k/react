486231898cb3be45dfa2ba9d99d138ca4cb0e3cb
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-07-16 16:45:48 +0100

    Route protection

--- 07-heroes-spa/src/auth/context/AuthProvider.jsx
+++ 07-heroes-spa/src/auth/context/AuthProvider.jsx
@@ -3,22 +3,33 @@ import {authReducer} from './authReducer';
 import {useReducer} from 'react';
 import {types} from '../types/types';
 
-const initialState = {
-    logged: false
-};
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    return {
+        logged: !!user,
+        user
+    }
+}
 
 export const AuthProvider = ({children}) => {
 
-    const [authState, dispatch] = useReducer(authReducer, initialState);
+    const [authState, dispatch] = useReducer(authReducer, {}, init);
 
     const login = (name = '') => {
+
+        const user = {
+            id: 'ABC',
+            name: name
+        };
+
         const action = {
             type: types.login,
-            payload: {
-                id: 'ABC',
-                name: name
-            }
+            payload: user
         }
+
+        localStorage.setItem('user', JSON.stringify(user));
+
         dispatch(action);
     };
 
