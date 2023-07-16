a75321495ec46498e9e881b925cdb9ea8deca27d
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-07-16 17:39:59 +0100

    Route protection

--- 07-heroes-spa/src/heroes/components/HeroCard.jsx
+++ 07-heroes-spa/src/heroes/components/HeroCard.jsx
@@ -11,7 +11,7 @@ export const HeroCard = ({
                              characters
                          }) => {
 
-    const heroImageUrl = `/assets/heroes/${id}.jpg`;
+    const heroImageUrl = `/public/heroes/${id}.jpg`;
 
     return (
         <div className="col animate__animated animate__fadeIn">
--- 07-heroes-spa/src/heroes/pages/HeroPage.jsx
+++ 07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -23,7 +23,7 @@ export const HeroPage = () => {
     return (
         <div className="row mt-5">
             <div className="col-4">
-                <img src={`/assets/heroes/${id}.jpg`}
+                <img src={`/public/heroes/${id}.jpg`}
                      alt={hero.superhero}
                      className="img-thumbnail animate__animated animate__fadeInLeft"/>
             </div>
