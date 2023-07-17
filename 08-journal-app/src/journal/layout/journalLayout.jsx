cf7dbcdc0a352e5461a8da052e8787299b5460b7
Javier Rojas <javier.rojas@sita.aero>
2023-07-17 11:57:04 +0100

    Journal App

--- 08-journal-app/src/journal/layout/journalLayout.jsx
+++ 08-journal-app/src/journal/layout/journalLayout.jsx
@@ -1,9 +1,12 @@
 import {Box} from '@mui/material';
+import {NavBar} from '../components';
 
 const drawerWidth = 240;
 export const JournalLayout = ({children}) => {
     return (
         <Box sx={{display: 'plex'}}>
+            <NavBar drawerWidth={drawerWidth}/>
+
             <Box component='main'
                  sx={{flexGrow: 1, p: 3}}>
                 {children}
