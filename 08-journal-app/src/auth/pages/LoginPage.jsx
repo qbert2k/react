5d461351f3edfc70cc252ae7c99c6d5fee4f3a99
Javier Rojas <javier.rojas@sita.aero>
2023-07-17 11:24:49 +0100

    Journal App

--- 08-journal-app/src/auth/pages/LoginPage.jsx
+++ 08-journal-app/src/auth/pages/LoginPage.jsx
@@ -1,4 +1,6 @@
-import {Grid, TextField, Typography} from '@mui/material';
+import {Link as RouterLink} from 'react-router-dom';
+import {Button, Grid, Link, TextField, Typography} from '@mui/material';
+import {Google} from '@mui/icons-material';
 
 export const LoginPage = () => {
     return (
@@ -29,6 +31,32 @@ export const LoginPage = () => {
                                        placeholder="password"
                                        fullWidth/>
                         </Grid>
+
+                        <Grid container spacing={2} sx={{mb: 2, mt: 1}}>
+                            <Grid item xs={12} sm={6}>
+                                <Button variant="contained"
+                                        fullWidth>
+                                    Login
+                                </Button>
+                            </Grid>
+
+                            <Grid item xs={12} sm={6}>
+                                <Button variant="contained"
+                                        fullWidth>
+                                    <Google/>
+                                    <Typography sx={{ml: 1}}>Google</Typography>
+                                </Button>
+                            </Grid>
+                        </Grid>
+
+                        <Grid container
+                              direction="row"
+                              justifyContent="end">
+                            <Link component={RouterLink} color="inherit" to="/suth/register">
+                                Register
+                            </Link>
+                        </Grid>
+
                     </Grid>
                 </form>
             </Grid>
