a8b76ce4944516edf94af48be392ae8905c07221
Javier Rojas <javier.rojas@sita.aero>
2023-07-17 11:14:32 +0100

    Journal App

--- 08-journal-app/src/auth/pages/LoginPage.jsx
+++ 08-journal-app/src/auth/pages/LoginPage.jsx
@@ -1,7 +1,37 @@
+import {Grid, TextField, Typography} from '@mui/material';
+
 export const LoginPage = () => {
     return (
-        <div>
-            LoginPage
-        </div>
+        <Grid container
+              spacing={0}
+              direction="column"
+              alignItems="center"
+              justifyContent="center"
+              sx={{minHeight: '100vh', backgroundColor: 'primary.main', padding: 4}}>
+            <Grid item className="box-shadow"
+                  xs={3}
+                  sx={{backgroundColor: 'white', padding: 3, borderRadius: 2}}>
+                <Typography variant="h5" sx={{mb: 1}}>
+                    Login
+                </Typography>
+
+                <form>
+                    <Grid container>
+                        <Grid item xs={12} sx={{mt: 2}}>
+                            <TextField label="Email"
+                                       type="email"
+                                       placeholder="email@example.com"
+                                       fullWidth/>
+                        </Grid>
+                        <Grid item xs={12} sx={{mt: 2}}>
+                            <TextField label="Password"
+                                       type="password"
+                                       placeholder="password"
+                                       fullWidth/>
+                        </Grid>
+                    </Grid>
+                </form>
+            </Grid>
+        </Grid>
     );
 }
