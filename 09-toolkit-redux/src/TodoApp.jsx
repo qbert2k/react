b89da0a43973f24789b046627f6eb366a44cb2bc
Javier Rojas <javier.rojas@sita.aero>
2023-07-20 13:47:50 +0100

    RTK

--- 09-toolkit-redux/src/TodoApp.jsx
+++ 09-toolkit-redux/src/TodoApp.jsx
@@ -2,6 +2,15 @@ export const TodoApp = () => {
     return (
         <>
             <h1>Todos - RKT Query</h1>
+            <hr/>
+            <h4>isLoading...</h4>
+
+            <pre>...</pre>
+
+            <button>
+                Next
+            </button>
+
         </>
     );
 }
--- 09-toolkit-redux/src/main.jsx
+++ 09-toolkit-redux/src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Provider} from 'react-redux';
-import {PokemonApp} from "./PokemonApp";
 import {store} from './store';
+import {PokemonApp} from './PokemonApp';
+import {TodoApp} from './TodoApp.jsx';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
-            <PokemonApp/>
+            {/*<PokemonApp/>*/}
+            <TodoApp/>
         </Provider>
     </React.StrictMode>,
 )
