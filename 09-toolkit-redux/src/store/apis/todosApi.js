91ef2c8a6d426476fa6a366aa12e5bf92922453f
Javier Rojas <javier.rojas@sita.aero>
2023-07-20 14:00:17 +0100

    RTK

--- 09-toolkit-redux/src/store/apis/todosApi.js
+++ 09-toolkit-redux/src/store/apis/todosApi.js
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-export const todosApi = () => ({
+export const todosApi = createApi({
     reducerPath: 'todos',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://jsonplaceholder.typicode.com'
