a0110d903931cbe405e5f2b4d9aa2bbc0e1b087f
Javier Rojas <javier.rojas@sita.aero>
2023-07-20 15:35:04 +0100

    Authentication: Firebase, Redux

---
+++ 08-journal-app/src/store/auth/authSlice.js
@@ -0,0 +1,29 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const initialState = {
+    status: 'checking', // not-authenticated, authenticated
+    uid: null,
+    email: null,
+    displayName: null,
+    photoURL: null,
+    errorMessage: null
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        login: (state, action) => {
+
+        },
+        logout: (state, payload) => {
+
+        },
+        checkingCredentials: (state) => {
+
+        },
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const {login, logout, checkingCredentials} = authSlice.actions;
