96fedaf0afbaa779f1c3f90ceef151f3fe49aff3
Javier Rojas <javier.rojas@sita.aero>
2023-07-20 16:03:27 +0100

    Authentication: Firebase, Redux

--- 08-journal-app/src/auth/pages/LoginPage.jsx
+++ 08-journal-app/src/auth/pages/LoginPage.jsx
@@ -1,29 +1,61 @@
+import {useDispatch} from 'react-redux';
 import {Link as RouterLink} from 'react-router-dom';
 import {Button, Grid, Link, TextField, Typography} from '@mui/material';
 import {Google} from '@mui/icons-material';
+
 import {AuthLayout} from '../layout/AuthLayout';
+import {useForm} from '../../hooks';
+import {checkingAuthentication, startGoogleSignIn} from '../../store/auth';
 
 export const LoginPage = () => {
+
+    const dispatch = useDispatch();
+
+    const {email, password, onInputChange} = useForm({
+        email: 'test@test.com',
+        password: '123456'
+    });
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log({email, password});
+
+        dispatch(checkingAuthentication());
+    };
+
+    const onGoogleSignIn = () => {
+        console.log('onGoogleSignIn');
+
+        dispatch(startGoogleSignIn());
+    };
+
     return (
         <AuthLayout title="Login">
-            <form>
+            <form onSubmit={onSubmit}>
                 <Grid container>
                     <Grid item xs={12} sx={{mt: 2}}>
                         <TextField label="Email"
                                    type="email"
-                                   placeholder="email@example.com"
-                                   fullWidth/>
+                                   placeholder="Email"
+                                   fullWidth
+                                   name="email"
+                                   value={email}
+                                   onChange={onInputChange}/>
                     </Grid>
                     <Grid item xs={12} sx={{mt: 2}}>
                         <TextField label="Password"
                                    type="password"
-                                   placeholder="password"
-                                   fullWidth/>
+                                   placeholder="Password"
+                                   fullWidth
+                                   name="password"
+                                   value={password}
+                                   onChange={onInputChange}/>
                     </Grid>
 
                     <Grid container spacing={2} sx={{mb: 2, mt: 1}}>
                         <Grid item xs={12} sm={6}>
-                            <Button variant="contained"
+                            <Button type="submit"
+                                    variant="contained"
                                     fullWidth>
                                 Login
                             </Button>
@@ -31,6 +63,7 @@ export const LoginPage = () => {
 
                         <Grid item xs={12} sm={6}>
                             <Button variant="contained"
+                                    onClick={onGoogleSignIn}
                                     fullWidth>
                                 <Google/>
                                 <Typography sx={{ml: 1}}>Google</Typography>
