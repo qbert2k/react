a70e8c9ddaf3413741812a9a81f169807e8caa03
Javier Rojas <javier.rojas@sita.aero>
2023-07-24 10:52:08 +0100

    Authentication: Firebase, Redux Persist Authentication

--- 08-journal-app/src/store/auth/authSlice.js
+++ 08-journal-app/src/store/auth/authSlice.js
@@ -1,7 +1,7 @@
-import {createSlice} from '@reduxjs/toolkit'
+import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = {
-    status: 'not-authenticated', // checking, not-authenticated, authenticated
+    status: 'checking', // checking, not-authenticated, authenticated
     uid: null,
     email: null,
     displayName: null,
