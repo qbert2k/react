6a8f1631449ce321419be92139d938f2a7b6f4dd
Javier Rojas <javier.rojas@sita.aero>
2023-07-24 11:55:18 +0100

    Authentication: Firebase, Redux Auth Custom Hook

--- 08-journal-app/src/router/AppRouter.jsx
+++ 08-journal-app/src/router/AppRouter.jsx
@@ -1,26 +1,12 @@
-import {useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
 import {Navigate, Route, Routes} from 'react-router-dom';
 import {AuthRoutes} from '../auth/routes/AuthRoutes';
-import {FirebaseAuth} from '../firebase/config';
 import {JournalRoutes} from '../journal/routes/JournalRoutes';
 import {CheckingAuth} from '../ui';
-import {onAuthStateChanged} from 'firebase/auth';
-import {login, logout} from '../store/auth';
+import {useCheckAuth} from '../hooks';
 
 export const AppRouter = () => {
 
-    const {status} = useSelector(state => state.auth);
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        onAuthStateChanged(FirebaseAuth, async (user) => {
-            if (!user) return dispatch(logout());
-
-            const {uid, email, displayName, photoURL} = user;
-            dispatch(login({uid, email, displayName, photoURL}));
-        });
-    }, []);
+    const status = useCheckAuth();
 
     if (status === 'checking') {
         return <CheckingAuth/>;
