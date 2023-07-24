c335a0b7f383f67faa085fc5a2b82c7565d7556e
Javier Rojas <javier.rojas@sita.aero>
2023-07-24 11:13:09 +0100

    Authentication: Firebase, Redux Persist Authentication

--- 08-journal-app/src/ui/components/CheckingAuth.jsx
+++ 08-journal-app/src/ui/components/CheckingAuth.jsx
@@ -8,7 +8,7 @@ export const CheckingAuth = () => {
               alignItems="center"
               justifyContent="center"
               sx={{minHeight: '100vh', backgroundColor: 'primary.main', padding: 4}}>
-            <Grid item
+            <Grid container
                   direction="row"
                   justifyContent="center">
                 <CircularProgress color="warning"/>
