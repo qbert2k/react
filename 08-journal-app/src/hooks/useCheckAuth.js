1631141ea70bf88498d6a6a66d9d34d5c4b8acbc
Javier Rojas <javier.rojas@sita.aero>
2023-07-24 15:47:24 +0100

    JournalApp: Redux, CRUD, Firestore, File Upload

--- 08-journal-app/src/hooks/useCheckAuth.js
+++ 08-journal-app/src/hooks/useCheckAuth.js
@@ -1,8 +1,9 @@
 import {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {onAuthStateChanged} from 'firebase/auth';
 import {FirebaseAuth} from '../firebase/config';
+import {onAuthStateChanged} from 'firebase/auth';
 import {login, logout} from '../store/auth';
+import {startLoadingNotes} from '../store/journal';
 
 export const useCheckAuth = () => {
 
@@ -15,6 +16,7 @@ export const useCheckAuth = () => {
 
             const {uid, email, displayName, photoURL} = user;
             dispatch(login({uid, email, displayName, photoURL}));
+            dispatch(startLoadingNotes());
         });
     }, []);
 
