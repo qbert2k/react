904909b067c89cd5f55bfb729af320410ccc99f2
Javier Rojas <javier.rojas@sita.aero>
2023-07-26 14:25:09 +0100

    Test JournalApp: Redux, Firebase, Firestore, Authentication

--- 08-journal-app/src/firebase/config.js
+++ 08-journal-app/src/firebase/config.js
@@ -5,6 +5,8 @@ import {getFirestore} from 'firebase/firestore/lite';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+console.log(import.meta.env);
+
 // Your web app's Firebase configuration
 // Dev / Prod
 // const firebaseConfig = {
