a4e7ac9dd83d91ac0d462ffcbab1ae67f20073f8
Javier Rojas <javier.rojas@sita.aero>
2023-07-26 17:10:35 +0100

    Test JournalApp: Redux, Firebase, Firestore, Authentication

--- 08-journal-app/src/firebase/config.js
+++ 08-journal-app/src/firebase/config.js
@@ -2,35 +2,26 @@
 import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore/lite';
-import {getEnvironments} from "../helpers/index.js";
+import {getEnvironments} from '../helpers';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const env = getEnvironments();
-console.log(env);
+const {
+    VITE_APIKEY,
+    VITE_AUTHDOMAIN,
+    VITE_PROJECTID,
+    VITE_STORAGEBUCKET,
+    VITE_MESSAGINGSENDERID,
+    VITE_APPID
+} = getEnvironments();
 
-// console.log(process.env);
-// console.log(import.meta.env);
-
-// Your web app's Firebase configuration
-// Dev / Prod
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAzpiurfhqTWxS162vZ1YaXA2C6Gl1w5Gs",
-//     authDomain: "react-tutorial-12101.firebaseapp.com",
-//     projectId: "react-tutorial-12101",
-//     storageBucket: "react-tutorial-12101.appspot.com",
-//     messagingSenderId: "845004634918",
-//     appId: "1:845004634918:web:3dede836348a67040f1fe7"
-// };
-
-// Testing
 const firebaseConfig = {
-    apiKey: "AIzaSyBELBXte6JBQzk3P4rtc86iyFOneU7CuTw",
-    authDomain: "react-tutorial-test-156f0.firebaseapp.com",
-    projectId: "react-tutorial-test-156f0",
-    storageBucket: "react-tutorial-test-156f0.appspot.com",
-    messagingSenderId: "909835949212",
-    appId: "1:909835949212:web:b865136d2f248a03818959"
+    apiKey: VITE_APIKEY,
+    authDomain: VITE_AUTHDOMAIN,
+    projectId: VITE_PROJECTID,
+    storageBucket: VITE_STORAGEBUCKET,
+    messagingSenderId: VITE_MESSAGINGSENDERID,
+    appId: VITE_APPID
 };
 
 // Initialize Firebase
