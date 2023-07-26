936e4b71fbb4c1e8fe8936db78024a64a3b91c16
Javier Rojas <javier.rojas@sita.aero>
2023-07-26 14:09:45 +0100

    Test JournalApp: Redux, Firebase, Firestore, Authentication

--- 08-journal-app/test/store/journal/thunks.test.js
+++ 08-journal-app/test/store/journal/thunks.test.js
@@ -1,4 +1,7 @@
 import {startNewNote} from '../../../src/store/journal/thunks';
+import {addNewEmptyNote, savingNewNote, setActiveNote} from '../../../src/store/journal/journalSlice';
+import {collection, deleteDoc, doc, getDocs} from 'firebase/firestore/lite';
+import {FirebaseDB} from '../../../src/firebase/config';
 
 describe('Test Journal thunk', function () {
 
@@ -10,7 +13,26 @@ describe('Test Journal thunk', function () {
     test('Should create a new note', async () => {
         const uid = 'TEST-UUID-123';
         getState.mockReturnValue({auth: {uid}});
+        const expectedNote = {
+            body: '',
+            title: '',
+            imageURL: [],
+            id: expect.any(String),
+            date: expect.any(Number)
+        }
 
         await startNewNote()(dispatch, getState);
+
+        expect(dispatch).toHaveBeenCalledWith(savingNewNote());
+        expect(dispatch).toHaveBeenCalledWith(addNewEmptyNote(expectedNote));
+        expect(dispatch).toHaveBeenCalledWith(setActiveNote(expectedNote));
+
+        // Clean
+        const collectionRef = collection(FirebaseDB, `${uid}/journal/notes`);
+        const docs = await getDocs(collectionRef);
+
+        const deletePromises = [];
+        docs.forEach(doc=>deletePromises.push(deleteDoc(doc.ref)))
+        await Promise.all(deletePromises);
     });
 });
