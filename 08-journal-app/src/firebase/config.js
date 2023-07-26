2e73b292a7d24608a33a05dbf6e546e6b4601e8c
Javier Rojas <javier.rojas@sita.aero>
2023-07-26 13:46:18 +0100

    Test JournalApp: Redux, Firebase, Firestore, Authentication

--- 08-journal-app/src/firebase/config.js
+++ 08-journal-app/src/firebase/config.js
@@ -6,13 +6,24 @@ import {getFirestore} from 'firebase/firestore/lite';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// Dev / Prod
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAzpiurfhqTWxS162vZ1YaXA2C6Gl1w5Gs",
+//     authDomain: "react-tutorial-12101.firebaseapp.com",
+//     projectId: "react-tutorial-12101",
+//     storageBucket: "react-tutorial-12101.appspot.com",
+//     messagingSenderId: "845004634918",
+//     appId: "1:845004634918:web:3dede836348a67040f1fe7"
+// };
+
+// Testing
 const firebaseConfig = {
-    apiKey: "AIzaSyAzpiurfhqTWxS162vZ1YaXA2C6Gl1w5Gs",
-    authDomain: "react-tutorial-12101.firebaseapp.com",
-    projectId: "react-tutorial-12101",
-    storageBucket: "react-tutorial-12101.appspot.com",
-    messagingSenderId: "845004634918",
-    appId: "1:845004634918:web:3dede836348a67040f1fe7"
+    apiKey: "AIzaSyBELBXte6JBQzk3P4rtc86iyFOneU7CuTw",
+    authDomain: "react-tutorial-test-156f0.firebaseapp.com",
+    projectId: "react-tutorial-test-156f0",
+    storageBucket: "react-tutorial-test-156f0.appspot.com",
+    messagingSenderId: "909835949212",
+    appId: "1:909835949212:web:b865136d2f248a03818959"
 };
 
 // Initialize Firebase
