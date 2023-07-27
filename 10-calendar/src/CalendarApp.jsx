a58e17a8f32d91d73b70427f03f47e54e6a9b830
Javier Rojas <javier.rojas@sita.aero>
2023-07-27 16:42:18 +0100

    Calendar: Structure and UI Design

---
+++ 10-calendar/src/CalendarApp.jsx
@@ -0,0 +1,5 @@
+export const CalendarApp = () => {
+  return (
+      <div>Calendar App</div>
+  );
+}
--- 10-calendar/src/main.jsx
+++ 10-calendar/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+
+import {CalendarApp} from './CalendarApp';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <CalendarApp/>
+    </React.StrictMode>
 )
