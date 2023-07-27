386859ae4d7ce45e3d9cfeced1f1dc7e842f450d
Javier Rojas <javier.rojas@sita.aero>
2023-07-27 17:28:49 +0100

    Calendar: Structure and UI Design

--- 10-calendar/src/calendar/components/Navbar.jsx
+++ 10-calendar/src/calendar/components/Navbar.jsx
@@ -1,13 +1,13 @@
 export const Navbar = () => {
   return (
-      <div className="Navbar navbar-dark bg-dark mb-4 px-4">
+      <div className="navbar navbar-dark bg-dark mb-4 px-4">
           <span className="navbar-brand">
               <i className="fas fa-calendar-alt"></i>
               &nbsp;
               Calendar
           </span>
 
-          <button className="">
+          <button className="btn btn-outline-danger">
               <i className="fas fa-sign-out-alt"></i>
           </button>
       </div>
--- 10-calendar/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,39 @@
+import {Calendar, dateFnsLocalizer} from 'react-big-calendar';
+import {addHours, format, getDay, parse, startOfWeek} from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+
 import {Navbar} from '../';
+
+const locales = {
+    'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+})
+
+const events = [{
+    title: 'My Birthday',
+    notes: 'Buy a cake',
+    start: new Date(),
+    end: addHours(new Date(), 2);
+}]
 
 export const CalendarPage = () => {
     return (
         <>
             <Navbar/>
+
+            <Calendar
+                // localizer={localizer}
+                //       events={myEventsList}
+                      startAccessor="start"
+                      endAccessor="end"
+                      style={{height: 500}}/>
         </>
     );
 }
