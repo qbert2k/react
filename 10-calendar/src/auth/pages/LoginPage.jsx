746b72bcb0a617aee5702c0ed5249d81248d24ec
Javier Rojas <javier.rojas@sita.aero>
2023-07-27 17:10:20 +0100

    Calendar: Structure and UI Design

--- 10-calendar/src/auth/pages/LoginPage.jsx
+++ 10-calendar/src/auth/pages/LoginPage.jsx
@@ -1,5 +1,61 @@
+import './LoginPage.css';
+
 export const LoginPage = () => {
-    return (
-        <div>Login Page</div>
-    );
+    return (<div className="container login-container">
+        <div className="row">
+            <div className="col-md-6 login-form-1">
+                <h3>Log In</h3>
+                <form>
+                    <div className="form-group mb-2">
+                        <input type="text"
+                               className="form-control"
+                               placeholder="Email"/>
+                    </div>
+                    <div className="form-group mb-2">
+                        <input type="password"
+                               className="form-control"
+                               placeholder="Password"/>
+                    </div>
+                    <div className="d-grid gap-2">
+                        <input type="submit"
+                               className="btnSubmit"
+                               value="Login"/>
+                    </div>
+                </form>
+            </div>
+
+            <div className="col-md-6 login-form-2">
+                <h3>Sign In</h3>
+                <form>
+                    <div className="form-group mb-2">
+                        <input type="text"
+                               className="form-control"
+                               placeholder="Name"/>
+                    </div>
+                    <div className="form-group mb-2">
+                        <input type="email"
+                               className="form-control"
+                               placeholder="Email"/>
+                    </div>
+                    <div className="form-group mb-2">
+                        <input type="password"
+                               className="form-control"
+                               placeholder="Password"/>
+                    </div>
+
+                    <div className="form-group mb-2">
+                        <input type="password"
+                               className="form-control"
+                               placeholder="Repeat Password"/>
+                    </div>
+
+                    <div className="d-grid gap-2">
+                        <input type="submit"
+                               className="btnSubmit"
+                               value="Register"/>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>)
 }
