5c99365b10aa4cd0ecb6b3ccacc667e6f221ac1a
Javier Rojas <javier.rojas@sita.aero>
2023-07-28 16:56:34 +0100

    Calendar: Structure and UI Design

--- 10-calendar/src/calendar/components/CalendarModal.jsx
+++ 10-calendar/src/calendar/components/CalendarModal.jsx
@@ -1,7 +1,8 @@
-import {useState} from 'react';
+import {useMemo, useState} from 'react';
 import Modal from 'react-modal';
 import {addHours, differenceInSeconds} from 'date-fns';
-
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 import DatePicker, {registerLocale} from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import es from 'date-fns/locale/es';
@@ -23,6 +24,7 @@ Modal.setAppElement('#root');
 
 export const CalendarModal = () => {
     const [isOpen, setIsOpen] = useState(true);
+    const [formSubmitted, setFormSubmitted] = useState(false);
 
     const [formValues, setFormValues] = useState({
         title: 'Javier',
@@ -30,6 +32,14 @@ export const CalendarModal = () => {
         start: new Date(),
         end: addHours(new Date, 2)
     });
+
+    const titleClass = useMemo(() => {
+        if (!formSubmitted) return '';
+
+        return (formValues.title.length > 0)
+            ? ''
+            : 'is-invalid';
+    }, [formValues.title, formSubmitted]);
 
     const onInputChange = ({target}) => {
         setFormValues({
@@ -52,10 +62,11 @@ export const CalendarModal = () => {
 
     const onSubmit = (event) => {
         event.preventDefault();
+        setFormSubmitted(true)
 
         const difference = differenceInSeconds(formValues.end, formValues.start);
         if (isNaN(difference) || difference <= 0) {
-            console.log('Date diff error');
+            Swal.fire('Date diff error', 'Review the start and end dates', "error");
             return;
         }
 
@@ -107,7 +118,7 @@ export const CalendarModal = () => {
                 <div className="form-group mb-2">
                     <label>Titulo y notas</label>
                     <input type="text"
-                           className="form-control"
+                           className={`form-control ${titleClass}`}
                            placeholder="Título del evento"
                            name="title"
                            value={formValues.title}
