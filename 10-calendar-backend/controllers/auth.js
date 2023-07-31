13e237a76da1da28a60326190d51e54c3695a1c6
Javier Rojas <javier.rojas@sita.aero>
2023-07-31 12:22:49 +0100

    Calendar App: Backend (Validator Middleware)

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,15 +1,6 @@
 const {response} = require('express');
-const {validationResult} = require('express-validator');
 
 const createUser = (req, res = response) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
-
     const {name, email, password} = req.body;
 
     res.status(201).json({
@@ -22,14 +13,6 @@ const createUser = (req, res = response) => {
 };
 
 const loginUser = (req, res = response) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
-
     const {email, password} = req.body;
 
     res.json({
--- 10-calendar-backend/routes/auth.js
+++ 10-calendar-backend/routes/auth.js
@@ -5,6 +5,8 @@
 const {Router} = require('express');
 const {check} = require('express-validator');
 const {createUser, loginUser, renewToken} = require('../controllers/auth');
+const {fieldValidator} = require('../middlewares/fieldValidator');
+
 const router = Router();
 
 router.post(
@@ -13,6 +15,7 @@ router.post(
         check('name', 'Name is required').not().isEmpty(),
         check('email', 'Email is required').isEmail(),
         check('password', 'Password should have at least 6 characters').isLength({min: 6}),
+        fieldValidator
     ], // middlewares
     createUser
 );
@@ -20,6 +23,7 @@ router.post(
 router.post('/', [
     check('email', 'Email is required').isEmail(),
     check('password', 'Password should have at least 6 characters').isLength({min: 6}),
+    fieldValidator
 ], loginUser);
 
 router.get('/renew', renewToken);
