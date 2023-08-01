45da3d4eeef98c013f403c227972c43008394740
Javier Rojas <javier.rojas@sita.aero>
2023-08-01 09:55:48 +0100

    Calendar App: Backend DB

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -33,18 +33,43 @@ const createUser = async (req, res = response) => {
             msg: 'Contact the administrator'
         });
     }
-
 };
 
-const loginUser = (req, res = response) => {
+const loginUser = async (req, res = response) => {
     const {email, password} = req.body;
 
-    res.json({
-        ok: true,
-        msg: 'loginUser',
-        email,
-        password
-    });
+    try {
+        const user = await User.findOne({email});
+
+        if (!user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'The user does not exist'
+            });
+        }
+
+        const validPassword = bcrypt.compareSync(password, user.password);
+
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Incorrect Password'
+            });
+        }
+
+        // TOdo JWT
+        res.json({
+            ok: true,
+            uid: user.uid,
+            email: user.email,
+            name: user.name
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Contact the administrator'
+        });
+    }
 };
 
 const renewToken = (req, res = response) => {
