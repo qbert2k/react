6fc3ac40248e27e2944595ddcc5cb48cd5c7eccd
Javier Rojas <javier.rojas@sita.aero>
2023-08-01 09:26:11 +0100

    Calendar App: Backend DB

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -2,15 +2,25 @@ const {response} = require('express');
 const User = require('../models/User')
 
 const createUser = async (req, res = response) => {
-    // const {name, email, password} = req.body;
+    const {email, password} = req.body;
 
     try {
-        const user = new User(req.body);
+        let user = await User.findOne({email});
+
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'User already exists'
+            });
+        }
+
+        user = new User(req.body);
         await user.save();
 
         res.status(201).json({
             ok: true,
-            msg: 'createUser'
+            uid: user.id,
+            name: user.name
         });
     } catch (error) {
         res.status(500).json({
