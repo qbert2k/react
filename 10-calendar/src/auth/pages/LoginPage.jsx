5ddfed35fa3807acaa42e291ce89219774bb67fc
Javier Rojas <javier.rojas@sita.aero>
2023-09-27 16:52:30 +0100

    Calendar App: Frontend

--- 10-calendar/src/auth/pages/LoginPage.jsx
+++ 10-calendar/src/auth/pages/LoginPage.jsx
@@ -1,20 +1,58 @@
 import './LoginPage.css';
+import {useForm} from '../../hooks';
+
+const loginFormFields = {
+    loginEmail: '',
+    loginPassword: ''
+};
+
+const registerFormFields = {
+    registerName: '',
+    registerEmail: '',
+    registerPassword: '',
+    registerPassword2: ''
+};
 
 export const LoginPage = () => {
+    const {loginEmail, loginPassword, onInputChange: onLoginInputChange} = useForm(loginFormFields);
+    const {
+        registerName,
+        registerEmail,
+        registerPassword,
+        registerPassword2,
+        onInputChange: onRegisterInputChange
+    } = useForm(registerFormFields);
+
+    const loginSubmit = (event) => {
+        event.preventDefault();
+        console.log({loginEmail, loginPassword});
+    };
+
+    const registerSubmit = (event) => {
+        event.preventDefault();
+        console.log({registerName, registerEmail, registerPassword, registerPassword2});
+    }
+
     return (<div className="container login-container">
         <div className="row">
             <div className="col-md-6 login-form-1">
                 <h3>Log In</h3>
-                <form>
+                <form onSubmit={loginSubmit}>
                     <div className="form-group mb-2">
                         <input type="text"
                                className="form-control"
-                               placeholder="Email"/>
+                               placeholder="Email"
+                               name="loginEmail"
+                               value={loginEmail}
+                               onChange={onLoginInputChange}/>
                     </div>
                     <div className="form-group mb-2">
                         <input type="password"
                                className="form-control"
-                               placeholder="Password"/>
+                               placeholder="Password"
+                               name="loginPassword"
+                               value={loginPassword}
+                               onChange={onLoginInputChange}/>
                     </div>
                     <div className="d-grid gap-2">
                         <input type="submit"
@@ -26,27 +64,39 @@ export const LoginPage = () => {
 
             <div className="col-md-6 login-form-2">
                 <h3>Sign In</h3>
-                <form>
+                <form onSubmit={registerSubmit}>
                     <div className="form-group mb-2">
                         <input type="text"
                                className="form-control"
-                               placeholder="Name"/>
+                               placeholder="Name"
+                               name="registerName"
+                               value={registerName}
+                               onChange={onRegisterInputChange}/>
                     </div>
                     <div className="form-group mb-2">
                         <input type="email"
                                className="form-control"
-                               placeholder="Email"/>
+                               placeholder="Email"
+                               name="registerEmail"
+                               value={registerEmail}
+                               onChange={onRegisterInputChange}/>
                     </div>
                     <div className="form-group mb-2">
                         <input type="password"
                                className="form-control"
-                               placeholder="Password"/>
+                               placeholder="Password"
+                               name="registerPassword"
+                               value={registerPassword}
+                               onChange={onRegisterInputChange}/>
                     </div>
 
                     <div className="form-group mb-2">
                         <input type="password"
                                className="form-control"
-                               placeholder="Repeat Password"/>
+                               placeholder="Repeat Password"
+                               name="registerPassword2"
+                               value={registerPassword2}
+                               onChange={onRegisterInputChange}/>
                     </div>
 
                     <div className="d-grid gap-2">
