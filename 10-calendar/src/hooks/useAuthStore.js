e000d05756bd5c8966851661392a4488e09f233a
Javier Rojas <javier.rojas@sita.aero>
2023-09-29 15:07:58 +0100

    Calendar App: Frontend - Load Events

--- 10-calendar/src/hooks/useAuthStore.js
+++ 10-calendar/src/hooks/useAuthStore.js
@@ -11,7 +11,7 @@ export const useAuthStore = () => {
         if (!token) return dispatch(onLogout());
 
         try {
-            const {data} = calendarApi.get('/auth/renew');
+            const {data} = await calendarApi.get('/auth/renew');
             localStorage.setItem('token', data.token);
             localStorage.setItem('token-init-date', new Date().getTime());
             dispatch(onLogin({name: data.name, uid: data.uid}));
