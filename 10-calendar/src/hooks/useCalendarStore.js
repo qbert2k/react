013a0d832ef32de67bd542d127bf4a8515849a9e
Javier Rojas <javier.rojas@sita.aero>
2023-09-29 11:32:44 +0100

    Calendar App: Frontend - Create Event

--- 10-calendar/src/hooks/useCalendarStore.js
+++ 10-calendar/src/hooks/useCalendarStore.js
@@ -1,9 +1,11 @@
 import {useDispatch, useSelector} from 'react-redux';
 import {onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent} from '../store';
+import {calendarApi} from '../api';
 
 export const useCalendarStore = () => {
     const dispatch = useDispatch();
     const {events, activeEvent} = useSelector(state => state.calendar);
+    const {user} = useSelector(state => state.auth);
 
     const setActiveEvent = (calendarEvent) => {
         dispatch(onSetActiveEvent(calendarEvent));
@@ -11,13 +13,16 @@ export const useCalendarStore = () => {
 
     const startSavingEvent = async (calendarEvent) => {
         if (calendarEvent._id) { // Update
+            // TODO: Update event
             dispatch(onUpdateEvent({
                 ...calendarEvent
             }));
         } else { // Create
+            const {data} = await calendarApi.post('/events', calendarEvent);
             dispatch(onAddNewEvent({
                 ...calendarEvent,
-                _id: new Date().getTime()
+                id: data.event.id,
+                user
             }));
         }
     };
