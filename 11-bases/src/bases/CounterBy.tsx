3e1b0aaed3b28b1806cb66510912cebf92e50b80
Javier Rojas <javier.rojas@sita.aero>
2023-10-05 14:54:42 +0100

    React TS

--- 11-bases/src/bases/CounterBy.tsx
+++ 11-bases/src/bases/CounterBy.tsx
@@ -6,26 +6,26 @@ interface Props {
 
 export const CounterBy = ({initialValue = 5}: Props) => {
 
-    const [counter, setCounter] = useState({
-        value: initialValue,
+    const [counterState, setCounterState] = useState({
+        counter: initialValue,
         clicks: 0
     });
 
-    const handleClick = (val = 1) => {
-        setCounter(prev => {
-                return {
-                    value: prev.value + val,
-                    clicks: prev.clicks + 1
-                };
-            }
+    const {counter, clicks} = counterState;
+
+    const handleClick = (value: number) => {
+        setCounterState(prev => ({
+                counter: prev.counter + value,
+                clicks: prev.clicks + 1
+            })
         )
     };
 
 
     return (
         <>
-            <h1>Counter By: {counter.value}</h1>
-            <h1>Clicks: {counter.clicks}</h1>
+            <h1>Counter By: {counter}</h1>
+            <h1>Clicks: {clicks}</h1>
 
             <button onClick={() => handleClick(1)}>+1</button>
             <button onClick={() => handleClick(5)}>+5</button>
