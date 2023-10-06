285dc86bad938eae7f51e9ef9521c26a20799a1f
Javier Rojas <javier.rojas@sita.aero>
2023-10-06 16:00:00 +0100

    useReducer

---
+++ 11-bases/src/bases/CounterReducerComponent.tsx
@@ -0,0 +1,53 @@
+import {useReducer, useState} from 'react';
+
+interface CounterState {
+    counter: number;
+    previous: number;
+    changes: number;
+}
+
+const INITIAL_STATE: CounterState = {
+    counter: 10,
+    previous: 15,
+    changes: 20
+}
+
+type CounterAction =
+    | { type: 'increaseBy', payload: { value: number } }
+    | { type: 'reset' };
+
+const counterReducer = (state: CounterState, action: CounterAction): CounterState => {
+    switch (action.type) {
+        case 'reset':
+            return {
+                counter: 0,
+                previous: 0,
+                changes: 0
+            };
+            break;
+        default:
+            return state;
+            break;
+    }
+};
+
+export const CounterReducerComponent = () => {
+
+    const [{counter}, dispatch] = useReducer(counterReducer, INITIAL_STATE);
+
+    const handleClick = () => {
+        dispatch({type: 'reset'});
+    }
+
+    return (
+        <>
+            <h1>
+                Counter Reducer: {counter}
+            </h1>
+
+            <button onClick={handleClick}>
+                Reset
+            </button>
+        </>
+    );
+}
