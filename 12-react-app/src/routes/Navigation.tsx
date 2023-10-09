243039944bfbf54e380e2606f38f44e7decbe22c
Javier Rojas <javier.rojas@sita.aero>
2023-10-09 12:20:00 +0100

    Lazy Load - Chunks - React Router DOM

--- 12-react-app/src/routes/Navigation.tsx
+++ 12-react-app/src/routes/Navigation.tsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Navigate, NavLink, Route, Routes} from "react-router-dom";
 import logo from "../logo.svg";
+import {LazyPage1, LazyPage2, LazyPage3} from "../01-lazyload/pages";
 
 export const Navigation = () => {
     return (
@@ -26,6 +27,25 @@ export const Navigation = () => {
                                 Users
                             </NavLink>
                         </li>
+
+                        <li>
+                            <NavLink to="/lazy1"
+                                     className={({isActive}) => isActive ? 'nav-active' : ''}>
+                                Lazy Page 1
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/lazy2"
+                                     className={({isActive}) => isActive ? 'nav-active' : ''}>
+                                Lazy Page 2
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/lazy3"
+                                     className={({isActive}) => isActive ? 'nav-active' : ''}>
+                                Lazy Page 3
+                            </NavLink>
+                        </li>
                     </ul>
                 </nav>
 
@@ -33,6 +53,10 @@ export const Navigation = () => {
                     <Route path="about" element={<h1>About Page</h1>}/>
                     <Route path="users" element={<h1>Users Page</h1>}/>
                     <Route path="home" element={<h1>Home Page</h1>}/>
+
+                    <Route path="lazy1" element={<LazyPage1/>}/>
+                    <Route path="lazy2" element={<LazyPage2/>}/>
+                    <Route path="lazy3" element={<LazyPage3/>}/>
 
                     <Route path="/*" element={<Navigate to="/home"/>}/>
                 </Routes>
