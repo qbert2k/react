67f516e6e434495ce08d4df5d4eb9a0decfb2061
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-11-07 19:49:58 +0000

    Control Props

---
+++ 12-react-app/src/02-component-patterns/pages/ShoppingPage2.tsx
@@ -0,0 +1,23 @@
+import {ProductButtons, ProductCard, ProductImage, ProductTitle} from "../components";
+import {products} from "../data/products";
+import "../styles/custom-styles.css";
+
+const product = products[0];
+
+export const ShoppingPage2 = () => {
+
+    return (
+        <div>
+            <h1>Shopping Store 2</h1>
+            <hr/>
+
+            <ProductCard key={product.id}
+                         product={product}
+                         className="bg-dark">
+                <ProductImage className="custom-image" style={{boxShadow: '10px 10px 10px rgba(0,0,0,0.2)'}}/>
+                <ProductTitle className="text-white text-bold"/>
+                <ProductButtons className="custom-buttons"/>
+            </ProductCard>
+        </div>
+    );
+}
