52252e73ed5a20cf709fe633955b1d9e90dd59ba
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-11-07 18:57:06 +0000

    Control Props

--- 12-react-app/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 12-react-app/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -31,19 +31,30 @@ export const ShoppingPage = () => {
     const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
 
     const onProductCountChange = ({count, product}: { count: number, product: Product }) => {
-
-        console.log(count);
-
         setShoppingCart(oldShoppingCart => {
-            if (count === 0) {
-                const {[product.id]: toDelete, ...rest} = oldShoppingCart;
-                return rest;
+            const productInCart: ProductInCart = oldShoppingCart[product.id] || {...product, count: 0};
+
+            if (Math.max(productInCart.count + count, 0) > 0) {
+                productInCart.count += count;
+                return {
+                    ...oldShoppingCart,
+                    [product.id]: productInCart
+                };
             }
 
-            return {
-                ...oldShoppingCart,
-                [product.id]: {...product, count}
-            };
+            // Remove product
+            const {[product.id]: toDelete, ...rest} = oldShoppingCart;
+            return rest;
+
+            // if (count === 0) {
+            //     const {[product.id]: toDelete, ...rest} = oldShoppingCart;
+            //     return rest;
+            // }
+            //
+            // return {
+            //     ...oldShoppingCart,
+            //     [product.id]: {...product, count}
+            // };
         });
     }
 
