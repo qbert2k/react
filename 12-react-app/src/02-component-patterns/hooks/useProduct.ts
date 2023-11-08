7c7e505af112860bc4d72fc4060ef0d416fe811e
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-11-08 19:57:34 +0000

    State Initializer + Function Child

--- 12-react-app/src/02-component-patterns/hooks/useProduct.ts
+++ 12-react-app/src/02-component-patterns/hooks/useProduct.ts
@@ -11,19 +11,30 @@ interface useProductArgs {
 export const useProduct = ({onChange, product, value = 0, initialValues}: useProductArgs) => {
     const [counter, setCounter] = useState<number>(initialValues?.count || value);
     const isControlled = useRef(!!onChange);
+    const isMounted = useRef(false);
 
     const increaseBy = (value: number) => {
         if (isControlled.current) {
             return onChange!({count: value, product});
         }
 
+        if (value > 0 && initialValues?.maxCount == counter) return ;
+
         const newValue = Math.max(counter + value, 0);
         setCounter(newValue);
+
+        onChange && onChange({count: newValue, product});
     }
 
     useEffect(() => {
+        if (!isMounted.current) return;
+
         setCounter(value);
     }, [value]);
+
+    // useEffect(() => {
+    //     isMounted.current = true;
+    // }, []);
 
     return {
         counter,
