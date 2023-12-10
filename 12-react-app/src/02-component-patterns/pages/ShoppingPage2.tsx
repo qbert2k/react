c51a77bb1347bae629e366fb9df4f92d95ab6409
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2023-12-10 13:04:27 +0000

    State Initializer + Function Child

--- 12-react-app/src/02-component-patterns/pages/ShoppingPage2.tsx
+++ 12-react-app/src/02-component-patterns/pages/ShoppingPage2.tsx
@@ -13,20 +13,25 @@ export const ShoppingPage2 = () => {
 
             <ProductCard key={product.id}
                          product={product}
-                         className="bg-dark"
+                         className="bg-dark text-white"
                          initialValues={{
                              count: 4,
                              maxCount: 10
                          }}>
                 {
-                    ({reset}) => (
+                    ({count, maxCount, isMaxCountReach, increaseBy, reset}) => (
                         <>
                             <ProductImage className="custom-image"
                                           style={{boxShadow: '10px 10px 10px rgba(0,0,0,0.2)'}}/>
-                            <ProductTitle className="text-white text-bold"/>
+                            <ProductTitle className="text-bold"/>
                             <ProductButtons className="custom-buttons"/>
 
                             <button onClick={reset}>Reset</button>
+                            <button onClick={() => increaseBy(-2)}>-2</button>
+                            {
+                                (!isMaxCountReach && <button onClick={() => increaseBy(2)}>+2</button>)
+                            }
+                            <span>{count} - {maxCount}</span>
                         </>
                     )
                 }
