0c312f8134f122849089207202821bf200d4b509
Javier Rojas <javier.rojas@sita.aero>
2024-01-04 14:29:11 +0000

    NPM Deploy

--- use-package/src/App.tsx
+++ use-package/src/App.tsx
@@ -1,10 +1,33 @@
 import './App.css';
+import {ProductButtons, ProductCard, ProductImage, ProductTitle} from 'jrb-product-card';
+import {Product} from "jrb-product-card/dist/interfaces/Interfaces";
+
+const product: Product = {
+    id: '1',
+    title: 'Coffe Mug',
+    img: './coffee-mug.png'
+}
 
 function App() {
     return (
         <div className="App App-header">
 
-            <h1>Hello World!</h1>
+            <ProductCard
+                product={product}
+                initialValues={{
+                    count: 6,
+                    maxCount: 10
+                }}>
+                {
+                    () => (
+                        <>
+                            <ProductImage/>
+                            <ProductTitle/>
+                            <ProductButtons/>
+                        </>
+                    )
+                }
+            </ProductCard>
 
         </div>
     );
