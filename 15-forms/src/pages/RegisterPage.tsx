4c261760d43ff5715f4e9077677c41a910215d4b
Javier Rojas <javier.rojas@sita.aero>
2024-01-05 14:31:48 +0000

    Forms

--- 15-forms/src/pages/RegisterPage.tsx
+++ 15-forms/src/pages/RegisterPage.tsx
@@ -4,16 +4,22 @@ import {ChangeEvent, useState} from "react";
 export const RegisterPage = () => {
 
     const [registerData, setRegisterData] = useState({
-        name: 'Javier',
-        email: 'javier.rojasblum@gmail.com',
-        password1: '123456',
-        password2: '123456',
+        name: '',
+        email: '',
+        password1: '',
+        password2: '',
     });
 
     const {name, email, password1, password2} = registerData;
 
     const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-        console.log(event.target.value);
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setRegisterData({
+            ...registerData,
+            [name]: value
+        });
     }
 
     return (
@@ -23,20 +29,24 @@ export const RegisterPage = () => {
             <form>
                 <input type="text"
                        placeholder="Name"
+                       name="name"
                        value={name}
                        onChange={onChange}/>
 
                 <input type="email"
                        placeholder="Email"
+                       name="email"
                        value={email}
                        onChange={onChange}/>
 
                 <input type="password"
                        placeholder="Password"
+                       name="password1"
                        value={password1}
                        onChange={onChange}/>
 
                 <input type="password"
+                       name="password2"
                        placeholder="Repeat Password"
                        value={password2}
                        onChange={onChange}/>
