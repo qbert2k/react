4fb7696931214efdfe1460138e0a032ae0c49ba5
Javier Rojas <javier.rojas@sita.aero>
2024-01-05 14:27:05 +0000

    Forms

--- 15-forms/src/pages/RegisterPage.tsx
+++ 15-forms/src/pages/RegisterPage.tsx
@@ -1,26 +1,50 @@
 import "../styles/styles.css";
+import {ChangeEvent, useState} from "react";
+
 export const RegisterPage = () => {
+
+    const [registerData, setRegisterData] = useState({
+        name: 'Javier',
+        email: 'javier.rojasblum@gmail.com',
+        password1: '123456',
+        password2: '123456',
+    });
+
+    const {name, email, password1, password2} = registerData;
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target.value);
+    }
+
     return (
-      <div>
-          <h1>Register Page</h1>
+        <div>
+            <h1>Register Page</h1>
 
-          <form>
-              <input type="text"
-                     placeholder="Name"/>
+            <form>
+                <input type="text"
+                       placeholder="Name"
+                       value={name}
+                       onChange={onChange}/>
 
-              <input type="email"
-                     placeholder="Email"/>
+                <input type="email"
+                       placeholder="Email"
+                       value={email}
+                       onChange={onChange}/>
 
-              <input type="password"
-                     placeholder="Password"/>
+                <input type="password"
+                       placeholder="Password"
+                       value={password1}
+                       onChange={onChange}/>
 
-              <input type="password"
-                     placeholder="Repeat Password"/>
+                <input type="password"
+                       placeholder="Repeat Password"
+                       value={password2}
+                       onChange={onChange}/>
 
-              <button type="submit">
-                  Create
-              </button>
-          </form>
-      </div>
+                <button type="submit">
+                    Create
+                </button>
+            </form>
+        </div>
     );
 };
