bc82591192726262c76ca0dc353fbf52002bb0b0
Javier Rojas <javier.rojas@sita.aero>
2024-01-08 16:15:41 +0000

    Dynamic Forms

--- 15-forms/src/pages/DynamicForm.tsx
+++ 15-forms/src/pages/DynamicForm.tsx
@@ -1,7 +1,34 @@
+import formJson from '../data/custom-form.json';
+import {Form, Formik} from "formik";
+import {MyTextInput} from "../components";
+
 export const DynamicForm = () => {
     return (
-        <>
+        <div>
             <h1>Dynamic Form</h1>
-        </>
+
+            <Formik
+                initialValues={{}}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}>
+                {
+                    (formik) => (
+                        <Form>
+                            {formJson.map(({type, name, value, label, placeholder}) => {
+                                return <MyTextInput
+                                    key={name}
+                                    type={(type as any)}
+                                    name={name}
+                                    label={label}
+                                    placeholder={placeholder}/>
+                            })}
+
+                            <button type="submit">Submit</button>
+                        </Form>
+                    )
+                }
+            </Formik>
+        </div>
     );
 }
