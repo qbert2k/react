2d07ab3ad3a191f227dbcc3d6d49ec28a5e94749
Javier Rojas <javier.rojas@sita.aero>
2024-01-09 16:22:27 +0000

    NPM Package Consumer

--- 17-package-consumer/src/App.tsx
+++ 17-package-consumer/src/App.tsx
@@ -1,29 +1,29 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {MyLabel} from "jrb-my-test-components";
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo"/>
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo"/>
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
 
+                <MyLabel label="Hello World!" allCaps color="text-primary"/>
 
-
-      </div>
-    </>
-  )
+            </div>
+        </>
+    )
 }
 
 export default App
