d5cc5e7bd1be601636714698ba8a27fbbb3ef419
Javier Rojas <javier.rojas@sita.aero>
2024-01-12 16:21:26 +0000

    React + PWA + Workbox

--- 18-pwa/react-calendar-frontend/src/sw-template.js
+++ 18-pwa/react-calendar-frontend/src/sw-template.js
@@ -5,7 +5,7 @@ importScripts(
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 const {registerRoute} = workbox.routing;
-const {CacheFirst} = workbox.strategies;
+const {CacheFirst, NetworkFirst} = workbox.strategies;
 
 registerRoute(
     new RegExp('https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'),
@@ -15,4 +15,14 @@ registerRoute(
 registerRoute(
     new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'),
     new CacheFirst()
+);
+
+registerRoute(
+    new RegExp('http://localhost:4000/api/auth/renew'),
+    new NetworkFirst()
+);
+
+registerRoute(
+    new RegExp('http://localhost:4000/api/events'),
+    new NetworkFirst()
 );
