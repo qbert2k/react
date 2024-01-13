ccbbb80fa5ff820ea030de05de2069ac8be51ee2
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2024-01-13 23:32:01 +0000

    Maps App

--- 19-maps-app/src/components/MapView.tsx
+++ 19-maps-app/src/components/MapView.tsx
@@ -1,10 +1,11 @@
 import {useContext, useLayoutEffect, useRef} from "react";
-import {PlacesContext} from "../context";
+import {MapContext, PlacesContext} from "../context";
 import {Loading} from "./";
 import {Map} from "mapbox-gl";
 
 export const MapView = () => {
     const {isLoading, userLocation} = useContext(PlacesContext);
+    const {setMap} = useContext(MapContext);
     const mapDiv = useRef<HTMLDivElement>(null);
 
     useLayoutEffect(() => {
@@ -15,6 +16,7 @@ export const MapView = () => {
                 center: userLocation,
                 zoom: 9,
             });
+            setMap(map);
         }
     }, [isLoading])
 
