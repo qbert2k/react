282610f75c7aed69102b261e6fa77fbb4a46f8d3
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2024-01-13 22:26:57 +0000

    Maps App

--- 19-maps-app/src/context/places/PlacesProvider.tsx
+++ 19-maps-app/src/context/places/PlacesProvider.tsx
@@ -1,6 +1,7 @@
-import {JSX, useReducer} from "react";
+import {JSX, useEffect, useReducer} from "react";
 import {PlacesContext} from "./PlacesContext";
 import {placesReducer} from "./placesReducer";
+import {getUserLocation} from "../../helpers";
 
 export interface PlacesState {
     isLoading: boolean;
@@ -18,6 +19,11 @@ interface Props {
 
 export const PlacesProvider = ({children}: Props) => {
     const [state, dispatch] = useReducer(placesReducer, INITIAL_STATE);
+
+    useEffect(() => {
+        getUserLocation()
+            .then(lngLat => dispatch({type: 'setUserLocation', payload: lngLat}));
+    }, []);
 
     return (
         <PlacesContext.Provider value={{
--- 19-maps-app/src/index.tsx
+++ 19-maps-app/src/index.tsx
@@ -6,6 +6,11 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
+if (!navigator.geolocation) {
+    alert('The browser has not Geolocation features');
+    throw new Error('The browser has not Geolocation features');
+}
+
 root.render(
     <React.StrictMode>
         <MapsApp/>
