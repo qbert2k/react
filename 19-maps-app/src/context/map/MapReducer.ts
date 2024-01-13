70b52aad10b8495453e61b6da9c7059787a05c3b
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2024-01-13 23:17:47 +0000

    Maps App

---
+++ 19-maps-app/src/context/map/MapReducer.ts
@@ -0,0 +1,11 @@
+import {Map} from "mapbox-gl";
+import {MapState} from "./MapProvider";
+
+type MapAction = { type: 'setMap', payload: Map };
+
+export const mapReducer = (state: MapState, action: MapAction): MapState => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
