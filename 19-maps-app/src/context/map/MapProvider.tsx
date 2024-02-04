1cb5de00c6c906c14d11fef9d4b5ff9f99734950
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2024-02-04 19:10:16 +0000

    Maps App

--- 19-maps-app/src/context/map/MapProvider.tsx
+++ 19-maps-app/src/context/map/MapProvider.tsx
@@ -1,4 +1,4 @@
-import {Map, Marker, Popup} from "mapbox-gl";
+import {AnySourceData, LngLatBounds, Map, Marker, Popup} from "mapbox-gl";
 import {MapContext} from "./MapContext";
 import {JSX, useContext, useEffect, useReducer} from "react";
 import {mapReducer} from "./MapReducer";
@@ -69,13 +69,63 @@ export const MapProvider = ({children}: Props) => {
     const getRouteBetweenPoints = async (start: [number, number], end: [number, number]) => {
         const resp = await directionsApi.get<PlacesResponse>(`/${start.join(',')};${end.join(',')}?`);
         const {distance, duration, geometry} = resp.data.routes[0];
+        const {coordinates: coords} = geometry;
 
         let kms = distance / 1000;
-        kms = Math.round(kms*100);
+        kms = Math.round(kms * 100);
         kms /= 100;
 
         const minutes = Math.floor(duration / 60);
         console.log({kms, minutes});
+
+        const bounds = new LngLatBounds(start, start);
+
+        for (const coord of coords) {
+            const newCoord: [number, number] = [coord[0], coord[1]];
+            bounds.extend(newCoord);
+        }
+
+        state.map?.fitBounds(bounds, {
+            padding: 200
+        });
+
+        // Polyline
+        const sourceData: AnySourceData = {
+            type: 'geojson',
+            data: {
+                type: 'FeatureCollection',
+                features: [
+                    {
+                        type: 'Feature',
+                        properties: {},
+                        geometry: {
+                            type: 'LineString',
+                            coordinates: coords
+                        }
+                    }
+                ]
+            }
+        };
+
+        if (state.map?.getLayer('RouteString')) {
+            state.map.removeLayer('RouteString');
+            state.map.removeSource('RouteString');
+        }
+
+        state.map?.addSource('RouteString', sourceData);
+        state.map?.addLayer({
+            id: 'RouteString',
+            type: 'line',
+            source: 'RouteString',
+            layout: {
+                'line-cap': 'round',
+                'line-join': 'round'
+            },
+            paint: {
+                'line-color': 'yellow',
+                'line-width': 3
+            }
+        })
     }
 
     return (
