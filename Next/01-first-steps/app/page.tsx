09ccaa1e8e1302f84a13694e07f16f53ba135167
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2024-02-13 19:11:10 +0000

    Next: ActiveLink

--- Next/01-first-steps/app/page.tsx
+++ Next/01-first-steps/app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function HomePage() {
     return (
         <main className="flex flex-col items-center p-24">
             <span className="text-5xl">Hello World!!!</span>
+            
+            <Link href={'/about'}>
+                About Page
+            </Link>
         </main>
     );
 }
